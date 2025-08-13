d0901eec58dce365c16b7289c50a93038abb79ad
Stieglerr <stieglerstr@hotmail.com>
2025-08-13 18:52:17 -0300

    2

--- src/app/model/contato.ts
+++ src/app/model/contato.ts
@@ -1,16 +1,22 @@
 export class Contato {
+    private _id: number;
     private _nome: string;
     private _telefone: string;
     private _dataNascimento?: string;
     private _genero?: string;
 
     constructor(nome: string, telefone: string, dataNascimento?: string, genero?: string) {
+        let chave = new Date();
+        this._id = chave.getTime();
         this._nome = nome;
         this._telefone = telefone;
         this._dataNascimento = dataNascimento;
         this._genero = genero;
     }
 
+    public get id():number{
+        return this._id;
+    }
     public get nome(): string {
         return this._nome;
     }
